db45260956460d26c16fb3678ce1a2510fe2a785
saienko-ana <saienko_ana@icloud.com>
2021-10-25 12:40:55 +0200

    did the INVITE section and a little bit of slider

--- js/script.js
+++ js/script.js
@@ -12,3 +12,36 @@ if (iconMenu) {
 		menuBody.classList.toggle('burger-active');
 	});
 }
+
+// -----------------------------------------------------------------------------
+// Табы из этого видео https://www.youtube.com/watch?v=Ta3RGu0bIhY
+// -----------------------------------------------------------------------------
+
+let tab = function () {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('is-active');
+        });
+        this.classList.add('is-active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
+
+};
+
+
+tab();
